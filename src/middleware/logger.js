7127e8322b1ad0192fc71aceb9c796c13c862f44
dar5o <87797682+dar5o@users.noreply.github.com>
2022-01-16 14:02:05 -0800

    Update logger.js

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,6 +1,6 @@
 'use strict';
 
 module.exports = (req, res, next)=>{
-  console.log(`method: ${req.method}, path: ${req.path}`);
+  console.log(`method used: ${req.method}, path used: ${req.path}`);
   next();
 };
